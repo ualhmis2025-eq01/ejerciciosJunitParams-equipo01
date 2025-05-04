f61a76e93b8efabca913304fbe7b806e5a300e52
Antonio Ordóñez <aom387@inlumine.ual.es>
2025-05-04 11:27:20 +0200

    Cobertura completa

--- sesion05Junit/src/ual/hmis/sesion05/ejercicio3/Ejercicio3.java
+++ sesion05Junit/src/ual/hmis/sesion05/ejercicio3/Ejercicio3.java
@@ -13,7 +13,7 @@ public class Ejercicio3 {
   public String ocultPassword(String password){
     if(password.length() < 5) {
       return "password demasiado corto";
-    } else if(password.length() >= 5 && password.length() <= 8) {
+    } else if(password.length() <= 8) {
       return "********";
     } else if(password.length() >= 12 && password.length() <= 40) {
       return "************";
